71e769fe21c339c5beef1738b48495163f61dc9e
unknown <rmtestard@gmail.com>
2019-03-01 14:15:30 +0100

    MDN

--- call_apply_bind.js
+++ call_apply_bind.js
@@ -1,4 +1,5 @@
 // Call
+// MDN : The call() method calls a function with a given this value and arguments provided individually.
 /*
 var obj = { num: 3 };
 
@@ -10,7 +11,8 @@ console.log(addToThis.call(obj, 2));
 */
 
 // Apply
-
+// MDN : The apply() method calls a function with a given this value, and arguments provided as an array (or an array-like object).
+/*
 var obj = { num: 3 };
 var arr = [1, 2, 3];
 
@@ -19,3 +21,16 @@ var addToThis = function(a, b, c) {
 };
 
 console.log(addToThis.apply(obj, arr));
+*/
+
+// Bind
+// MDN : The bind() method creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
+// var obj = { num: 2 };
+
+// var addToThis = function(a, b, c) {
+//   return this.num + a + b + c;
+// };
+
+// var bound = addToThis.bind(obj);
+
+// console.log(bound(1, 2, 3));
